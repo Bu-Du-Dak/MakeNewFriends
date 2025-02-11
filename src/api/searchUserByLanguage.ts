941a4959b1a6e4fb1609e20c34bec29d8eaec2cb
Bu-Du-Dak <guri930219@gmail.com>
2025-02-12 08:02:27 +0900

    팔로우 필터 추가

--- src/api/searchUserByLanguage.ts
+++ src/api/searchUserByLanguage.ts
@@ -1,17 +1,24 @@
+import { getMyFollowing } from "./getMyFollowing";
 import { githubClient } from "./githubClient";
 
 export const searchUserByLanguage = async (
   language: string,
   maxResult: number = 10
 ) => {
+  const myFollowing = await getMyFollowing();
+
   try {
     const response = await githubClient.get(`/search/users`, {
       params: {
-        q: `language:${language} followers:>100`,
+        q: `language:${language} followers:>10`,
         per_page: maxResult,
       },
     });
-    const users = response.data.items.map((user: any) => user.login);
+    const users = response.data.items
+      .map((user: any) => user.login)
+      .filter((user: string) => !myFollowing.includes(user))
+      .slice(0, maxResult);
+
     return users;
   } catch (e) {
     console.error("❌ 유저 찾기 실패 >> ", e);
